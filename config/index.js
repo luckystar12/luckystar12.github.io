b576b894177d78fa03d17005fa98e85c6bac78e5
luckystar12 <liyuemei@ifanr.com>
2019-05-20 17:44:33 +0800

    修改作品集

--- config/index.js
+++ config/index.js
@@ -2,15 +2,19 @@ window.worksData = [
   {
     type: 'normal',
     title: '糖纸小程序(处于上线前外测阶段)',
-    date: '2018.03-至今',
+    date: '2019.03-至今',
     images: [
       {
         type: 'vertical',
         url: './images/index/tangzhi-0.png',
       },
+      {
+        type: 'vertical',
+        url: './images/index/tangzhi-1.png',
+      },
     ],
-    content: '该项目定位为“产品社区”：以产品信息为基础的产品社区，用产品和内容连接用户以及生产者，核心是获取关键标签的用户群。',
-    responsibility: '主要负责此产品的数据表设计，部分核心 UI 开发',
+    content: '该项目定位为“产品社区”：以产品信息为基础的产品社区，用产品和内容连接用户以及生产者，核心是获取关键标签的用户群。核心功能： ① 电子产品专业评测，链接相关文章，用户进行点评或问答； ② APP 应用程序说明，价格曲线图，点评等；',
+    responsibility: '责任描述：参与此产品的数据表设计，开发大部分核心 UI。该项目用到 Canvas 画海报， HTML富文本标签转小程序标签等',
   },
   {
     type: 'normal',
@@ -30,11 +34,19 @@ window.worksData = [
       },
     ],
     content: '国内聚焦创新及消费科技领域的线上第一媒体，我们倾注自己的努力，打造全方位及体系化的产品及服务，帮助人们更好地获得科技带来的改变和乐趣。',
-    responsibility: '主要负责 web 端和移动端的主页，和接入第四范式相关文章等',
+    responsibility: '责任描述：主要负责 web 端和移动端的主页开发，接入第四范式推荐文章，GA 埋点，微信公众号微信排版等',
   },
   {
     type: 'normal',
-    title: '<a target="_blank" href="https://www.ifanr.com/api/ifanr-special/huawei-watch-gt">华为手表 HUAWEI Watch GT 不插电生活</a>',
+    title: '爱范儿官方运营后台',
+    date: '2018.03-至今',
+    images: [],
+    content: '搭建新运营后台，将 Wordpress 核心功能模块搬移到新运营后台，逐步摆脱对 Wordpress 的依赖。主要模块有：文章、栏目、广告、专题、H5 活动页、评论等模块。项目主要用 React + Redux + React Router + Webpack 技术框架。',
+    responsibility: '责任描述：主要负责搭建项目脚手架及其栏目模块、专题模块、广告模块、H5 活动专题页模块的开发和维护',
+  },
+  {
+    type: 'normal',
+    title: '<a target="_blank" href="https://www.ifanr.com/api/ifanr-special/huawei-watch-gt">华为手表 · HUAWEI Watch GT 「不插电生活」</a>',
     date: '2018.12',
     images: [
       {
@@ -45,8 +57,8 @@ window.worksData = [
         url: './images/index/huawei-2.png',
       },
     ],
-    content: '此商务项目承接「华为手表」专题业务',
-    responsibility: '主要负责「华为手表」主题移动端，亮点：融合了竖屏翻页和横屏翻页、视频、DOMToIMG生成海报图片分享，PV/UV 记录，特殊字体引入、微信分享等等',
+    content: '承接华为第三方 H5 商务项目，主题「不插电生活」',
+    responsibility: '责任描述：负责该主题移动端。亮点：融合了竖屏翻页和横屏翻页、视频、DOMToIMG生成海报图片分享，PV/UV 记录，特殊字体引入、微信分享等等',
   },
   {
     type: 'normal',
@@ -61,8 +73,8 @@ window.worksData = [
         url: './images/index/jd-1.png',
       },
     ],
-    content: '此商务项目承接京东「11.11」专题业务',
-    responsibility: '亮点：融合音频，解决兼容性问题；动态控制游戏开始，问题生成，记录回答选项，最后统计出游戏结果，推出用户的性格象征',
+    content: '承接京东第三方 H5 商务项目，京东双十一「星机体验官」主题',
+    responsibility: '责任描述：移动端，亮点：融合音频，解决兼容性问题；动态控制游戏开始，问题生成，记录回答选项，最后统计出游戏结果，推出用户的性格象征',
   },
   {
     type: 'normal',
@@ -80,12 +92,12 @@ window.worksData = [
         url: './images/index/hp-2.png',
       },
     ],
-    content: '此商务项目承接惠普共三期的专题业务, 第一期：https://www.ifanr.com/api/ifanr-special/zhizaozhe; 第二期：https://www.ifanr.com/api/ifanr-special/hp2; 第三期：https://www.ifanr.com/api/ifanr-special/hp3',
+    content: '承接惠普第三方 H5 商务项目，共三期, 第一期：https://www.ifanr.com/api/ifanr-special/zhizaozhe; 第二期：https://www.ifanr.com/api/ifanr-special/hp2; 第三期：https://www.ifanr.com/api/ifanr-special/hp3',
     responsibility: '亮点：web 端采取了视觉视差实现页面滚动，解决了不同浏览器鼠标监听事件兼容性（event.wheelData 或 event.detail）、滚动值差异；移动端采取竖页翻页',
   },
   {
     type: 'normal',
-    title: '微信支付8.8（移动端）',
+    title: '<a target="_blank" href="https://www.ifanr.com/api/ifanr-special/round-table-forum">微信支付日 「8.8」(移动端)</a>',
     date: '2018.08',
     images: [
       {
@@ -96,8 +108,8 @@ window.worksData = [
         url: './images/index/round-1.png',
       },
     ],
-    content: '此商务项目承接微信支付「8.8」专题业务',
-    responsibility: '主要负责微信支付圆桌会议主题 https://www.ifanr.com/api/ifanr-special/round-table-forum',
+    content: '承接 2018 微信支付日 H5 商务项目, 有三个主题：圆桌论坛（https://www.ifanr.com/api/ifanr-special/round-table-forum），微信支付纪录片（https://www.ifanr.com/api/ifanr-special/payment-documentary），微信支付品牌 TVC（https://www.ifanr.com/api/ifanr-special/wechat-payment-history）',
+    responsibility: '责任描述：主要负责微信支付圆桌会议主题。',
   },
 ]
 
